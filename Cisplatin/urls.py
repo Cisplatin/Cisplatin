19a8a2426965ce6dbb78c333a91e07d4620082d6
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-03-16 18:42:56 -0700

    removed useless files

--- Cisplatin/urls.py
+++ Cisplatin/urls.py
@@ -14,10 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
 from Cisplatin_App.views import home
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^', home.home),
 ]
