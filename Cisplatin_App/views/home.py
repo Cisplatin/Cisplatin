42784b796cf0c993890b3453d3baf9f6cb6771a0
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-03-18 22:27:06 -0700

    added relative paths

--- Cisplatin_App/views/home.py
+++ Cisplatin_App/views/home.py
@@ -1,19 +1,21 @@
-from os.path import basename, getsize
+from os.path import basename, getsize, join
 from wsgiref.util import FileWrapper
 
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from Cisplatin.settings import BASE_DIR
+
 # Render the homepage, with PGP key
 def home(request):
     # Load up the PGP key
-    with open("static/pgp/public-key.asc", "r") as public:
+    with open(join(BASE_DIR, "static/pgp/public-key.asc"), "r") as public:
         key = public.read()
     return render(request, 'home.html', {'PGP_KEY' : key})
 
 # Return Simon's resume
 def resume(request):
-    filename = "static/pdf/Simon Hajjar.pdf"
+    filename = join(BASE_DIR, "static/pdf/Simon Hajjar.pdf")
     wrapper = FileWrapper(file(filename))
     response = HttpResponse(wrapper, content_type='application/pdf')
     response['Content-Disposition'] = 'attachment; filename="%s"' % basename(filename)
